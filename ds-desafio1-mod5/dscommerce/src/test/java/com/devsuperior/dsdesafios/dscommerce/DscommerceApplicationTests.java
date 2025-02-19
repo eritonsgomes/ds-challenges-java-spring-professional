4da8ded4a39c94f4fd67feb65b00271a19460b09
Eriton S. Gomes <eritonsgomes@gmail.com>
2025-02-19 17:48:56 -0300

    Projeto DSCommerce Criado

---
+++ ds-desafio1-mod5/dscommerce/src/test/java/com/devsuperior/dsdesafios/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdesafios.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
